c10dae19759990b82b67117eb528529ea1b79b4f
Lillian Adele Cerise <lili.cerise@yale.edu>
2019-01-06 06:16:45 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,30 @@
 def division(num1, num2)
- 
+ num1/num2
 end
 
 def assign_variable(value)
-  
+  assign_variable(value) = value
 end
 
 def argue
-  
+  argue
 end
 
-def greeting
-
+def greeting(greeting, name)
+  "#{greeting}, #{name}"
 end
 
 def return_a_value
+  return_a_value = "Nice"
 
 end
 
 def last_evaluated_value
+  last_evaluated_value = "expert"
 
 end
 
 def pizza_party
+  pizza_party = "cheese"
 
 end
